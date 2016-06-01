1db4a91f08a7fb6ae043c3f0745c46082a16957f
John Clayton <johnc@filewave.com>
2016-06-01 13:42:00 +0200

    first commit

---
+++ app/pages/location/location.ts
@@ -0,0 +1,14 @@
+import {Page, NavController} from 'ionic-angular';
+
+/*
+  Generated class for the LocationPage page.
+
+  See http://ionicframework.com/docs/v2/components/#navigation for more info on
+  Ionic pages and navigation.
+*/
+@Page({
+  templateUrl: 'build/pages/location/location.html',
+})
+export class LocationPage {
+  constructor(public nav: NavController) {}
+}
